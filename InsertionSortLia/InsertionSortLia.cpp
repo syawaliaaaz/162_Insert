da7a2566aa6a8c4c1f10f8f4b0f7d088b37d463e
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-03-14 14:19:56 +0700

    Membuat Prosedur dari Insertion Sort

--- InsertionSortLia/InsertionSortLia.cpp
+++ InsertionSortLia/InsertionSortLia.cpp
@@ -31,4 +31,34 @@ void input() { //Procedure Input
 	}
 }
 
+void insertionsort() { //Procedure Insertionsort
+
+	int temp; //Membuat variable data temporer atau penyimpanan sementara
+	int i, j; //Membuat variable j sebgai penanda
+
+	for (i = 1; i <= n - 1; i++) { //1.Looping dengan i dimulai dari 1 hingga n-1
+
+		temp = arr[i];             //2.Simpan nilai arr[i] ke variable sementara temp
+
+		j = i - 1;                 //3.Setting nilai j sama dengan i-1
+
+		while (j >= 0 && arr[j] > temp) //4.Looping while dimana nilai j lebih besar sama dengan 0 dan
+			                           //arr[1] lebih besar darpada temp
+		{
+			arr[j + 1] = arr[j];   //4a.Simpan arr[j] ke dalam variable arr[j+1]
+			j--;                   //4b.Decrement nilai j by 1
+		}
+
+		arr[j + 1] = temp;         //5.Simpan nilai temp ke dalam arr[j+1]
+
+		cout << "\nPass " << i << ": "; //Output ke layar
+		for (int k = 0; k < n; k++) {   //Looping nilai k dimulai dari 0 hingga n-1
+			cout << arr[k] << " ";      //Output ke layar
+
+		}
+
+	}
+
+}
+
 
